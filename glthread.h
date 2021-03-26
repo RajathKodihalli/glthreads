367e5c898406668caf1b3841f689534428bc5925
Rajath.k <rajath@ficustexas.com>
2021-03-26 11:04:04 +0530

    glthread.h completed

--- glthread.h
+++ glthread.h
@@ -25,8 +25,26 @@ glthread_remove(glthread_t *lst, glthread_node_t *glnode);
 
 
 /* Iterative macro to Iterate over GLThreads */
+#define ITERATE_GL_THREADS_BEGIN(lsptr, struct_type, ptr)	\
+{								\
+	glthread_node_t *_glnode = NULL, *_next = NULL;		\
+	for(_glnode = lsptr->head; -glnode; _gload = _next){	\
+	_next = _glnode->right;					\
+	ptr = (struct_type *)((char *)_glnode - lsptr->offset);	
 
+#define ITERATE_GL_THREADS_ENDS }}
 
+#define glthread_node_init(glnode)	\
+	glnode->left = NULL;		\
+	glnode->right = NULL;
+
+void
+init_glthread(glthread_t *glthread, unsigned int offset);
+
+#define offsetof(struct_name, field_name)	\
+	((unsigned int)&((struct_name *)0)->field_name)
+
+#endif /* __GLTHREADS__ */
 
 
 
